e7a6ddab0317b53195b223f48a12156a3cea3a4e
HOVOH <71901758+HOVOH@users.noreply.github.com>
2021-11-23 16:58:10 -0500

    force dark mode

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -22,12 +22,13 @@ const useTheme = (): [string, (e: KeyboardEvent) => void, boolean] => {
   };
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme");
+    /*    const localTheme = window.localStorage.getItem("theme");
     window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches && !localTheme
       ? setMode("dark")
       : localTheme
       ? setTheme(localTheme)
-      : setMode("light");
+      : setMode("light");*/
+    setMode("dark");
     setMounted(true);
   }, []);
 
