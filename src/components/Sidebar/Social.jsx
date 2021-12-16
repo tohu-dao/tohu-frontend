3413a061c9be06ee70cc0029bc7d808062141d41
melondrone <melondronez@gmail.com>
2021-12-17 00:54:51 +0800

    [EXOD-027] UI improvements, disable bond button when quantity > balance

--- src/components/Sidebar/Social.jsx
+++ src/components/Sidebar/Social.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as GitHub } from "../../assets/icons/github.svg";
 import { ReactComponent as Medium } from "../../assets/icons/medium.svg";
 import { ReactComponent as Twitter } from "../../assets/icons/twitter.svg";
 import { ReactComponent as Discord } from "../../assets/icons/discord.svg";
+import { ReactComponent as Telegram } from "../../assets/icons/telegram.svg";
 
 export default function Social() {
   return (
@@ -11,11 +12,9 @@ export default function Social() {
         <SvgIcon color="primary" component={GitHub} />
       </Link>
 
-      {/*
-      <Link href="https://olympusdao.medium.com/" target="_blank">
+      <Link href="https://exodiafi.medium.com/" target="_blank">
         <SvgIcon color="primary" component={Medium} />
       </Link>
-*/}
 
       <Link href="https://twitter.com/EXODIAFinance" target="_blank">
         <SvgIcon color="primary" component={Twitter} />
@@ -23,6 +22,10 @@ export default function Social() {
 
       <Link href="https://discord.gg/exodia" target="_blank">
         <SvgIcon color="primary" component={Discord} />
+      </Link>
+
+      <Link href="https://t.co/nmK6cc8F8X" target="_blank">
+        <SvgIcon color="primary" component={Telegram} />
       </Link>
     </div>
   );
