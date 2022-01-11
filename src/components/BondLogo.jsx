09a8bc0d2b137247c6697d5c15baa07c1cdae466
melondrone <melondronez@gmail.com>
2022-01-12 00:56:04 +0800

    [EXOD-000] Add px back

--- src/components/BondLogo.jsx
+++ src/components/BondLogo.jsx
@@ -7,7 +7,7 @@ function BondLogo({ bond }) {
   // Need more space if its an LP token
   if (bond.isLP) {
     viewBox = "0 0 64 32";
-    style = { height: "32px", width: "64" };
+    style = { height: "32px", width: "64px" };
   }
 
   if (bond.isMonolith) {
