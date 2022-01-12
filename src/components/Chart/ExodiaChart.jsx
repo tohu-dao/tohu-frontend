a7d088fe9d31097d3ce0044590ddf6a35dc4fd25
melondrone <melondronez@gmail.com>
2022-01-13 03:17:46 +0800

    [EXOD-000] Remove console log

--- src/components/Chart/ExodiaChart.jsx
+++ src/components/Chart/ExodiaChart.jsx
@@ -523,10 +523,8 @@ export const ExodiaPieChart = withChartCard(
 );
 
 export const trimNumber = number => {
-  if (Number(number) > 1000000) {
-    console.log(number);
-    return `${parseFloat(number) / 1000000}M`;
-  } else if (Number(number) > 1000) return `${parseFloat(number) / 1000}k`;
+  if (Number(number) > 1000000) return `${parseFloat(number) / 1000000}M`;
+  else if (Number(number) > 1000) return `${parseFloat(number) / 1000}k`;
   return number;
 };
 
