bbfe612f27d24c345ab03b10c33eee11d284736a
HOVOH <71901758+HOVOH@users.noreply.github.com>
2022-01-24 21:46:44 -0500

    move fBEETS bond to active

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -1,11 +1,10 @@
 import { StableBond, LPBond, CustomBond } from "src/lib/Bond";
 import { dai, wftm, the_monolith_lp, the_monolith_lp_old, gohm, fbeets, rock } from "./BondDefinitions";
 
-
 // Add new bonds to this array!!
-export const allBonds: (StableBond | CustomBond | LPBond)[] = [dai, wftm, gohm, the_monolith_lp];
+export const allBonds: (StableBond | CustomBond | LPBond)[] = [dai, wftm, gohm, the_monolith_lp, fbeets];
 export const allExpiredBonds: (StableBond | CustomBond | LPBond)[] = [the_monolith_lp_old];
-export const allUpcomingBonds: (StableBond | CustomBond | LPBond)[] = [fbeets, rock];
+export const allUpcomingBonds: (StableBond | CustomBond | LPBond)[] = [rock];
 
 export const allBondsMap = allBonds.reduce((prevVal, bond) => {
   return { ...prevVal, [bond.name]: bond };
