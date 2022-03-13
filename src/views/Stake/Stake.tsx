4954b1ad9fa6a04fc5c283208d3cde8e58d666d0
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-03-13 16:34:17 +1100

    (🍞, 🍞)

--- src/views/Stake/Stake.tsx
+++ src/views/Stake/Stake.tsx
@@ -194,7 +194,7 @@ function Stake() {
           <Grid container direction="column" spacing={2}>
             <Grid item>
               <div className="card-header">
-                <Typography variant="h5">Single Stake (3, 3)</Typography>
+                <Typography variant="h5">Single Stake (🍞, 🍞)</Typography>
                 <RebaseTimer />
               </div>
             </Grid>
